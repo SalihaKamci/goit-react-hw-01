de2164d0d34bad5ceca94e7b589fb292504852af
saliha <salihakamci06@gmail.com>
2025-10-01 20:31:41 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import './App.css'
-import Profile  from './components/Profile/profile'
-import userData from './components/Profile/userData.json'
-import FriendList from './components/FriendList/friends'
-import listFrend from './components/FriendList/friends.json'
-
+import "./App.css";
+import Profile from "./components/Profile/profile";
+import userData from "./components/Profile/userData.json";
+import FriendList from "./components/FriendList/friends";
+import listFrend from "./components/FriendList/friends.json";
+import TransactionHistory from "./components/transactions/TransactionHistory";
+import transaction from "./components/transactions/transactions.json";
 function App() {
-
-
   return (
     <>
-     <Profile
-     name={userData.username}
-     tag={userData.tag}
-     location={userData.location}
-     image= {userData.avatar}
-     stats={userData.stats}
-
-  
-     />
-   <FriendList ListFrends ={listFrend}/>
-    
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList ListFrends={listFrend} />
+      <TransactionHistory items={transaction} />
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
